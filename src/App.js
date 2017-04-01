7b07fbfd4a88d69ff86768e42687e723cdc61033
Klas Mellbourn <klas.mellbourn@klarna.com>
2017-04-01 20:36:21 +0200

    add code demonstrating css-grid
    Inspired by https://css-tricks.com/css-grid-one-layout-multiple-ways/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <header>Header</header>
+        <aside className="sidebar-left">Left aside</aside>
+        <article>Article Lorem ipsum dolor sit amet, eius graeci vivendum usu et. Quo in doming epicurei, ex nec ignota ridens. No pro quem principes. Qui enim idque sapientem an, pro expetendis theophrastus ei, eum liber pertinax in.
+
+Ullum albucius sit te, quo singulis principes moderatius et, eu wisi utroque liberavisse per. Ius quot case prodesset eu, unum iisque facilisis pri ei. Volumus maiestatis in sed. Ea sit animal luptatum signiferumque, et vim habemus oporteat. Facete iriure sapiente</article>
+        <aside className="sidebar-right">Right aside</aside>
+        <footer>Footer</footer>
       </div>
     );
   }
